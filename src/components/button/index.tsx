72c0b7865d2dfb8ca7d2453aaa2eaa3ee7531e99
Leonardo Leenen <leonardoleenen@gmail.com>
2022-02-13 10:07:13 -0300

    First scratch version

---
+++ src/components/button/index.tsx
@@ -0,0 +1,36 @@
+import { MouseEventHandler } from 'react'
+import React, { FC } from 'react'
+
+interface ButtonProps {
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary?: boolean
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string
+    /**
+     * How large should the button be?
+     */
+    size?: 'small' | 'medium' | 'large'
+    /**
+     * Button contents
+     */
+    label: string
+    /**
+     * Optional click handler
+     */
+    onClick?: MouseEventHandler<HTMLButtonElement>
+}
+export const Button = ({
+    primary = false,
+    size = 'medium',
+    backgroundColor,
+    label,
+    ...props
+}: ButtonProps) => {
+    return <button className="btn btn-primary">Un boton</button>
+}
+
+export default Button
