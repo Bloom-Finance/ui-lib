75d69f5e0523454ed56eae257afa82f3f7aa1854
Maria Noel Leenen <leenen.maria@gmail.com>
2022-02-16 18:55:39 -0300

    .

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import Icon from '../icon'
-import Container from '../container'
-import Label from '../label'
+import { MouseEventHandler } from 'react'
+import React, { FC } from 'react'
 
-interface Props {
+interface AlertProps {
     status?: 'warning' | 'default' | 'error'
-    subtitle?: string
-    icon?: any
+    label: string
+    icon?: JSX.Element
 }
+export const Alert = ({
+    status = 'default',
+    label,
+    icon = <div></div>,
+    ...props
+}: AlertProps) => {
+    let getSize = () => {
+        switch (status) {
+            case 'warning':
+                return 'text-3xl'
+            case 'error':
+                return 'text-2xl'
 
-const Component = (props: Props): JSX.Element => {
+            default:
+                return 'text-base'
+        }
+    }
+
+    let classNameBase =
+        'mx-auto items-center undefined grid place-items-strech rounded-none text-white px-4 py-5'
+    classNameBase =
+        classNameBase + (status === 'default' ? ' bg-black' : ' bg-red-600')
+
     return (
-        <div className="mx-auto items-center undefined grid place-items-strech rounded-none">
-            <div
-                className={`px-4 py-5  ${props.status === 'default' && 'bg-black'} ${
-                    props.status === 'warning' && 'bg-yellow-500'
-                } ${props.status === 'error' && 'bg-red-600'}`}
-            >
-                <div className='font-semibold'>
-                    {props.icon}
-                    <Label type='small' color="white">{props.subtitle}</Label>
-                </div>
-            </div>
+        <div className={classNameBase}>
+            <div>{label}</div>
         </div>
     )
 }
 
-export default Component
+export default Alert
