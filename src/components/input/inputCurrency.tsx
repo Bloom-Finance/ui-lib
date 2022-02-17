47e82a767f9eec4f7411f66decb67c2443a78c19
alexFiorenza <alexfiorenza2012@gmail.com>
2022-02-17 18:33:53 -0300

    Modified input currency

--- src/components/input/inputCurrency.tsx
+++ src/components/input/inputCurrency.tsx
@@ -14,21 +14,6 @@ interface Props {
 }
 const InputCurrency = (props: Props): JSX.Element => {
     return (
-<<<<<<< HEAD
-        <div className={`${props.className}`}>
-            <div className="stat place-items-center place-content-center">
-                <div className="stat-value flex font-normal ">
-                    <input
-                        autoFocus={props.autoFocus || false}
-                        type="number"
-                        ref={props.controller?.ref}
-                        onChange={props.controller?.onChange}
-                        className="w-5/6 text-center"
-                        placeholder={props.placeholder}
-                    ></input>
-                    <span className=" text-gray-500">{props.currency}</span>
-                </div>
-=======
         <div className="flex justify-center">
             <div className="flex items-center gap-x-3">
                 <input
@@ -39,7 +24,6 @@ const InputCurrency = (props: Props): JSX.Element => {
                     placeholder={props.placeholder}
                 ></input>
                 <span className="text-4xl text-gray-400">{props.currency}</span>
->>>>>>> 3f642d1f6c0abd982db249e62db5773250024c55
             </div>
         </div>
     )
