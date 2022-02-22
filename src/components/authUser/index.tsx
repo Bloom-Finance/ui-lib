d5d6ff69742b6095ba98b24f33de7e4c62276b50
Leonardo Leenen <leonardoleenen@gmail.com>
2022-02-22 12:09:52 -0300

    Clean Code

--- src/components/authUser/index.tsx
+++ src/components/authUser/index.tsx
@@ -16,15 +16,9 @@ export const AuthUser = ({ userEmail, userName, ...props }: AuthUserProps) => {
             <Avatar label="" />
             <div>
                 <h2 className="text-gray-900 text-base font-semibold">
-<<<<<<< HEAD
-                    {props.userName}
-                </h2>
-                <h4 className="text-sm text-gray-500">{props.email}</h4>
-=======
                     {userName}
                 </h2>
                 <h4 className="text-sm text-gray-500">{userEmail}</h4>
->>>>>>> 78d4d5cba2303a57df8ec6fc6dbaf3a2c64e8646
             </div>
         </div>
     )
