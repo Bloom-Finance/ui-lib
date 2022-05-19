f8a862c7fd499db5ce297ea08f628973ca0a7000
Juan Germano <germi@hey.com>
2022-05-19 16:29:58 -0300

    ui

--- src/controls/nav/index.tsx
+++ src/controls/nav/index.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import Icon from '../../components/icon'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import style from './style.module.scss'
 
-interface Props {}
+interface Props {
+    label: string
+    route: string
+    merchantLogo: string
+}
 
 const Component = (props: Props): JSX.Element => {
+    const router = useRouter()
     return (
-        <header>
-            <div>
-                <Icon
-                    stroke={1}
-                    type="CHEVRON-BACK"
-                    className="absolute left-4 top-10 -translate-y-1/2"
-                ></Icon>
-            </div>
-            <div>
-                <img
-                    src={order.merchant.company_logo}
-                    alt={order.merchant.company}
-                    className="block w-48 m-auto mb-6"
-                />
-            </div>
-        </header>
+        <div className={style.main}>
+            <header>
+                <div
+                    className="flex items-center"
+                    onClick={() => router.push(props.route)}
+                >
+                    <Icon stroke={1} type="CHEVRON-BACK" className=""></Icon>
+                    <span>{props.label}</span>
+                </div>
+                <div>
+                    <img src={props.merchantLogo} />
+                </div>
+            </header>
+        </div>
     )
 }
 
